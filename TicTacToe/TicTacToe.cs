0ba330fbb5e56bdbeb890b73379700de2dd199ad
Sandeepshinde630 <sandeepshinde630@gmail.com>
2021-09-01 09:31:55 +0530

    Add project files.

---
+++ TicTacToe/TicTacToe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+   public class TicTacToe
+    {
+        char[] board = new char[10];
+        public char[] Board()
+        {
+            for (int i = 1; i < board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
+        }
+    }
+}
